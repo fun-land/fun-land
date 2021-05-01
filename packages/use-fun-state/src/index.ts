bafac5b55124199eea4bc25c4b2ed9381f22be19
Jethro Larson <jethrolarson@gmail.com>
2021-05-01 15:46:55 -0700

    Creating new module for react adapter of fun-state

---
+++ packages/use-fun-state/src/index.ts
@@ -0,0 +1,21 @@
+import {useState, useMemo, useRef} from 'react'
+import {pureState, FunState} from '@fun-land/fun-state'
+
+/** Create react-hooks-based FunState */
+export default function useFunState<State>(initialState: State): FunState<State> {
+  // create the mutable state engine
+  const ref = useRef(initialState)
+  // creating an unused react state engine just to get rerender to happen when state changes
+  const [_, setState] = useState(initialState)
+  return useMemo(
+    () =>
+      pureState({
+        getState: () => ref.current,
+        modState: (f) => {
+          ref.current = f(ref.current)
+          setState(ref.current)
+        }
+      }),
+    [ref]
+  )
+}
