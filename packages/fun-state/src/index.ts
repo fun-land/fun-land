489fd044c89bc9d094522b1fb258e4dc3ed785cc
Jethro Larson <jethrolarson@gmail.com>
2021-05-01 13:32:59 -0700

    remove bad import of fun-state

--- packages/fun-state/src/index.ts
+++
@@ -1,3 +0,0 @@
-export {default} from './useFunState'
-export {FunState, Updater} from './FunState'
-export * from './fun'
--- packages/fun-state/src/useFunState.ts
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react'
-import {pureState, FunState} from './FunState'
-
-/** Create react-hooks-based FunState */
-export default function useFunState<State>(initialState: State): FunState<State> {
-  // create the mutable state engine
-  const [current, modState] = useState(initialState)
-  return pureState({getState: () => current, modState})
-}
